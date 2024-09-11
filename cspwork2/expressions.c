50d8795f6eaf9bc6b46bce7be93bc904fe35aa26
CephasPetersen <cephas.petersen@ucas-edu.net>
2024-09-11 23:07:58 +0000

    finished expression c

--- cspwork2/expressions.c
+++ cspwork2/expressions.c
@@ -1,5 +1,50 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 int main(void) {
-    return 0;
+    int q = 1;
+    int w = 2;
+    int e = 3;
+    int r = 4;
+    int t = 5;
+    int y = 6; 
+    int u = 7; 
+    int i = 8;
+    int o = 9; 
+    int p = 12; 
+    int a = 15; 
+    int s = 17; 
+    int d = 18; 
+    int f = 22; 
+    int g = 24; 
+    int h = 72;
+    int sum;
+    int sumtwo;
+    int sumthree;
+    int sumfour;
+    int sumfive;
+    int sumsix;
+    int sumseven;
+    int sumeight;
+    int sumnine;
+    sum = u-g/i*r+y;
+    printf("sum = %d", sum);
+    sumtwo = d/e-u+w*t;
+    printf("sumtwo = %d", sumtwo);
+    sumthree = y*r/p+h/i-o;
+    printf("sumthree = %d", sumthree);
+    sumfour = s-y/w+r*e;
+    printf("sumfour = %d", sumfour);
+    sumfive = -w*(q*r-w/w)+(y+w-e);
+    printf("sumfive = %d", sumfive);
+    sumsix = -q*abs((e-r*u)/t)-w*g/y;
+    printf("sumsix = %d", sumsix);
+    sumseven = (3*pow(5,2)/15)-(5-pow(2,2));
+    printf("sumseven %d", sumseven);
+    sumeight = (pow(1,4)*pow(2,2)+pow(3,3))-pow(2,5)/4;
+    printf("sumeight = %d", sumeight);
+    sumnine = pow((22/2-2*5),2)+pow((4-6/6),2);
+    printf("sumnine = %d", sumnine);
+    return 0; 
 }
