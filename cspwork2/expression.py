c691ba6756bbdfab70bc0388969f566ef1aadcf5
CephasPetersen <cephas.petersen@ucas-edu.net>
2024-09-11 20:05:04 +0000

    save 1

---
+++ cspwork2/expression.py
@@ -0,0 +1,23 @@
+numi = 1
+numii = 2
+numiii = 3
+numiv = 4
+numv = 5
+numvi = 6
+numvii = 7
+numviii = 8
+numix = 9
+numxii = 12
+numxv = 15
+numxvii = 17
+numxviii = 18
+numxxii = 22
+numxxiv = 24
+nuxxxxxxxii = 72
+
+print(7-24/8*4+6)
+print(18/3-7+2*5)
+print(6*4/12+72/8-9)
+print((17-6/2)+4*3)
+print(-2)
+-2(1*4-2/2)+(6+2-3)
