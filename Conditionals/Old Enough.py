6de522387e0d6834205bee8a810f98259b7577c9
CephasPetersen <cephas.petersen@ucas-edu.net>
2024-10-01 20:21:31 +0000

    did ol py

---
+++ Conditionals/Old Enough.py
@@ -0,0 +1,11 @@
+age = 12
+if age >= 18:
+    print("You can vote unc")
+elif age >= 16:
+    print("You can drive unc")
+elif age >= 15:
+    print("You get get a learners permit unc")
+elif age >= 6:
+    print("You can go to school lil bro")
+else:
+    print("Sorry lil bro")
--- Functions/Hello.py
+++
@@ -1,7 +0,0 @@
-def hello(name):
-   print(f"hello {name}")
-hello("Roberto")
-hello("Cephas")
-hello("louie")
-hello("tony")
-hello("tim")
