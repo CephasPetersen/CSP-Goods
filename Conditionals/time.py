63386052972f76b694c0a8a1b92a613798596f71
CephasPetersen <cephas.petersen@ucas-edu.net>
2024-10-02 20:39:30 +0000

    did timepy

--- Conditionals/time.py
+++ Conditionals/time.py
@@ -4,3 +4,11 @@ now = datetime.datetime.now().hour
 
 print (now)
 
+if now <= 12:
+    print("good morning brother\n")
+elif now <= 18:
+    print("good afternoon brother\n")
+elif now <= 20:
+    print("good evening brother\n")
+else:
+    print("good night brother\n")
